c04672c2825df460e7faefd6b0cf7f2b7288c123
nnsnico <yuji.jampan.blue@gmail.com>
2023-06-11 00:52:03 +0900

    fix: add function to transform from DTO to data class

--- datasource/src/main/java/net/nns/keywd/datasource/entity/DiaryEntity.kt
+++ datasource/src/main/java/net/nns/keywd/datasource/entity/DiaryEntity.kt
@@ -3,10 +3,22 @@ package net.nns.keywd.datasource.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import arrow.core.Either
+import net.nns.keywd.model.Diary
+import net.nns.keywd.model.Title
 
 @Entity
 data class DiaryEntity(
     @PrimaryKey val id: Int,
     @ColumnInfo val title: String,
     @ColumnInfo val content: String,
-)
+) {
+    suspend fun toDiary(): Either<Throwable, Diary> =
+        Title.fromString(title).map {
+            Diary(
+                title = it,
+                content = content,
+            )
+        }
+
+}
