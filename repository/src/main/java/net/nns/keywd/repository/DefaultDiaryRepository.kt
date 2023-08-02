557c17917ce10df8db3541e8e66a62393b95d48b
nnsnico <yuji.jampan.blue@gmail.com>
2023-08-02 22:59:21 +0900

    chore: move traverse from legacy arrow-kt

--- repository/src/main/java/net/nns/keywd/repository/DefaultDiaryRepository.kt
+++ repository/src/main/java/net/nns/keywd/repository/DefaultDiaryRepository.kt
@@ -2,7 +2,7 @@ package net.nns.keywd.repository
 
 import arrow.core.Either
 import arrow.core.continuations.either
-import arrow.core.traverse
+import net.nns.keywd.core.traverse
 import net.nns.keywd.datasource.dao.DiaryDao
 import net.nns.keywd.datasource.dto.DiaryEntity
 import net.nns.keywd.model.Diary
@@ -15,8 +15,6 @@ class DefaultDiaryRepository(private val diaryDao: DiaryDao) : DiaryRepository {
     override suspend fun deleteDiary(diary: Diary): Either<Throwable, Unit> =
         Either.catch { diaryDao.delete(DiaryEntity.fromDiary(diary)) }
 
-    // TODO: Make traverse as utility function because it is deprecated
-    // @see: https://github.com/arrow-kt/arrow/blob/722a7c003db8c6612e190492cafbbc8f5e659851/arrow-libs/core/arrow-core/src/commonMain/kotlin/arrow/core/Iterable.kt#L299
     override suspend fun getSavedDiaries(): Either<Throwable, List<Diary>> = either {
         val entities = Either.catch { diaryDao.getAllDiaryContents() }.bind()
         val diaryList = entities.traverse { it.toDiary() }.bind()
