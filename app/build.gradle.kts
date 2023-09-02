3ce4ad0a10526bb1e79630131d863b56c074a0a0
nnsnico <yuji.jampan.blue@gmail.com>
2023-09-03 00:17:10 +0900

    feat: upgradle compose dependencies based on kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,11 +38,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.toVersion(versions.compileJvm.get())
+        targetCompatibility = JavaVersion.toVersion(versions.compileJvm.get())
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = versions.compileJvm.get()
     }
     buildFeatures {
         compose = true
