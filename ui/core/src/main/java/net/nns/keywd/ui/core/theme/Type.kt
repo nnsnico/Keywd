14fc20a61eeac2a166d71f0c9f322827b278205b
nnsnico <yuji.jampan.blue@gmail.com>
2023-09-09 20:46:19 +0900

    feat: add custom font (shirokuma font)

--- ui/core/src/main/java/net/nns/keywd/ui/core/theme/Type.kt
+++ ui/core/src/main/java/net/nns/keywd/ui/core/theme/Type.kt
@@ -1,18 +1,30 @@
 package net.nns.keywd.ui.core.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import net.nns.keywdui.core.R
+
+private val defaultFontFamily = FontFamily(Font(R.font.shirokuma_regular))
+private val defaultTypography = Typography()
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-    ),
+    displayLarge = defaultTypography.displayLarge.copy(fontFamily = defaultFontFamily),
+    displayMedium = defaultTypography.displayMedium.copy(fontFamily = defaultFontFamily),
+    displaySmall = defaultTypography.displaySmall.copy(fontFamily = defaultFontFamily),
+    headlineLarge = defaultTypography.headlineLarge.copy(fontFamily = defaultFontFamily),
+    headlineMedium = defaultTypography.headlineMedium.copy(fontFamily = defaultFontFamily),
+    headlineSmall = defaultTypography.headlineSmall.copy(fontFamily = defaultFontFamily),
+    titleLarge = defaultTypography.titleLarge.copy(fontFamily = defaultFontFamily),
+    titleMedium = defaultTypography.titleMedium.copy(fontFamily = defaultFontFamily),
+    titleSmall = defaultTypography.titleSmall.copy(fontFamily = defaultFontFamily),
+    bodyLarge = defaultTypography.bodyLarge.copy(fontFamily = defaultFontFamily),
+    bodyMedium = defaultTypography.bodyLarge.copy(fontFamily = defaultFontFamily),
+    bodySmall = defaultTypography.bodyMedium.copy(fontFamily = defaultFontFamily),
+    labelLarge = defaultTypography.labelLarge.copy(fontFamily = defaultFontFamily),
+    labelMedium = defaultTypography.labelMedium.copy(fontFamily = defaultFontFamily),
+    labelSmall = defaultTypography.labelSmall.copy(fontFamily = defaultFontFamily),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
