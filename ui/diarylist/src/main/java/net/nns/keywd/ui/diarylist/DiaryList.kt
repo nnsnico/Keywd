30daf2713c1e2cb6e867ec2539552f91b00d5ea9
nnsnico <yuji.jampan.blue@gmail.com>
2023-09-21 00:57:24 +0900

    chore: tweak TopAppBar design

--- ui/diarylist/src/main/java/net/nns/keywd/ui/diarylist/DiaryList.kt
+++ ui/diarylist/src/main/java/net/nns/keywd/ui/diarylist/DiaryList.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -20,7 +22,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -89,11 +90,17 @@ fun DiaryListLayout(
             }
         },
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "夢日記")
-                },
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+            ) {
+                Spacer(modifier = Modifier.heightIn(min = 96.dp))
+                Text(
+                    text = "夢日記",
+                    style = MaterialTheme.typography.headlineMedium,
+                )
+            }
         },
         contentWindowInsets = WindowInsets.zero(),
     ) {
