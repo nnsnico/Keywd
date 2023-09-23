0c9a704c60107ef6e0737159f9233d83b877fd29
nnsnico <yuji.jampan.blue@gmail.com>
2023-09-23 11:55:53 +0900

    fix: can't save Keyword class to instance state

--- model/src/main/java/net/nns/keywd/model/Keyword.kt
+++ model/src/main/java/net/nns/keywd/model/Keyword.kt
@@ -1,8 +1,11 @@
 package net.nns.keywd.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import net.nns.keywd.core.NonEmptyString
 
+@Parcelize
 data class Keyword(
     val id: String,
     val value: NonEmptyString,
-)
+) : Parcelable
