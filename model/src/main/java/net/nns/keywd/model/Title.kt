b1938576f03517b0db0c59152842f3054a53e877
nnsnico <yuji.jampan.blue@gmail.com>
2023-12-17 18:04:45 +0900

    chore: refactor `fromDate` to type safe

--- model/src/main/java/net/nns/keywd/model/Title.kt
+++ model/src/main/java/net/nns/keywd/model/Title.kt
@@ -3,20 +3,26 @@ package net.nns.keywd.model
 import arrow.core.Either
 import arrow.core.Option
 import arrow.core.continuations.either
+import net.nns.keywd.core.NonEmptyString
 import java.time.LocalDate
 
 @JvmInline
 value class Title private constructor(val value: String) {
     companion object {
         suspend fun fromDate(date: LocalDate): Either<Throwable, Title> = either {
-            Title(date.toString())
+            val maybeDate = NonEmptyString.init(date.toString())
+            maybeDate.map {
+                Title(it.value)
+            }.bind {
+                Exception("Title is empty")
+            }
         }
 
         suspend fun fromString(str: String): Either<Throwable, Title> = either {
             val maybeDate = Either.catch { LocalDate.parse(str) }.bind()
-            val date = Option.fromNullable(maybeDate).toEither {
+            val date = Option.fromNullable(maybeDate).bind {
                 Exception("Can't parse to Date")
-            }.bind()
+            }
 
             fromDate(date).bind()
         }
